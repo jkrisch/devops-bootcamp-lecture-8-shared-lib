daadefaba8129b7f1d564e97c32d4dda2709c065
Jonas <jonas.krisch@gmail.com>
2023-05-24 15:37:52 +0200

    bugfixing

--- vars/commitGitChanges.groovy
+++ vars/commitGitChanges.groovy
@@ -1,10 +1,11 @@
 def call(String credentialsId, String repoUrl){
     withCredentials([string(credentialsId: "${credentialsId}",variable:'TOKEN')]){
+        def newRepoUrl = $repoUrl.replace("https://","https://$TOKEN@")
         sh """
         git config --global user.email jenkins@myexample.com
         git config --global user.name jenkins
 
-        git remote set-url origin $repoUrl.replace("https://","https://$TOKEN@")
+        git remote set-url origin $newRepoUrl
         git add .
         git commit -m 'version upgrade to ${env.NEW_VERSION}'
         git push origin HEAD:new-version
